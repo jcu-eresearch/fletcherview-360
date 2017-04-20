3a4392972500468c3fc50297df9d4e06c7796c29
David Beitey <david@davidjb.com>
2017-04-20 15:19:44 +1000

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,262 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fletcherview-paddock",
+      "name": "Fletcherview Paddock",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.37311171467125703,
+        "pitch": -0.06838518699193585,
+        "fov": 0.6956887957706618
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3098780942415083,
+          "pitch": 0.007472408551793208,
+          "rotation": 25.918139392115812,
+          "target": "1-lolworth-creek"
+        },
+        {
+          "yaw": -2.225135082267869,
+          "pitch": -0.05820843197993497,
+          "rotation": 11.780972450961727,
+          "target": "2-lava-flow-basalt"
+        },
+        {
+          "yaw": 0.8026395957190893,
+          "pitch": -0.04976658256131827,
+          "rotation": 0,
+          "target": "4-main-farm-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lolworth-creek",
+      "name": "Lolworth Creek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4353361669588409,
+        "pitch": 0.004431019257530977,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9425942709472803,
+          "pitch": -0.0017073925416273283,
+          "rotation": 0,
+          "target": "0-fletcherview-paddock"
+        },
+        {
+          "yaw": -2.3215037024836818,
+          "pitch": 0.00962689068559719,
+          "rotation": 11.780972450961727,
+          "target": "2-lava-flow-basalt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lava-flow-basalt",
+      "name": "Lava Flow (Basalt)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.131659992045698,
+        "pitch": -0.09823265307248974,
+        "fov": 1.795700431959519
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1346962382329302,
+          "pitch": 0.03704626152564572,
+          "rotation": 0,
+          "target": "0-fletcherview-paddock"
+        },
+        {
+          "yaw": 0.12683957906627086,
+          "pitch": 0.051201854595001706,
+          "rotation": 0.7853981633974483,
+          "target": "1-lolworth-creek"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cattle-yards",
+      "name": "Cattle Yards",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0707955609302964,
+        "pitch": 0.05387129651534117,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-main-farm-house",
+      "name": "Main Farm House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8639435599180487,
+        "pitch": -0.13109949031363755,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7377371889674755,
+          "pitch": -0.05009328019412429,
+          "rotation": 0.7853981633974483,
+          "target": "0-fletcherview-paddock"
+        },
+        {
+          "yaw": 1.426890613157017,
+          "pitch": -0.04642812387559658,
+          "rotation": 0,
+          "target": "5-round-yards--sheds"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-round-yards--sheds",
+      "name": "Round Yards & Sheds",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6347114900268807,
+        "pitch": 0.11963751995335237,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6278508613494314,
+          "pitch": 0.00956060212140386,
+          "rotation": 0,
+          "target": "4-main-farm-house"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Fletcherview Research Station",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
